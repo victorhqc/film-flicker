474a07622711c3edb2c1727198c2ca70c37ffbeb
Victor Quiroz <git@victorhqc.com>
2024-09-14 23:40:26 +0200

    chore: format

--- build.rs
+++ build.rs
@@ -43,9 +43,9 @@ where
     for entry in it {
         let path = entry.path();
         let name = path.strip_prefix(&src).unwrap();
-        let path_string = name
-            .to_str()
-            .map(str::to_owned).context(BadPathSnafu { path: format!("{:?}", name) })?;
+        let path_string = name.to_str().map(str::to_owned).context(BadPathSnafu {
+            path: format!("{:?}", name),
+        })?;
 
         if path.is_file() {
             println!("adding file {path:?} as {name:?} ...");
